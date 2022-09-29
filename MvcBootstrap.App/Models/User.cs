6b375d332468886c2e8b4e8f1f62c8455ff969f0
Daniel <danlpsarcanjo@gmail.com>
2022-09-29 01:15:29 -0400

    Extend IdentityUser with User

---
+++ MvcBootstrap.App/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MvcBootstrap.App.Models
+{
+    public class User : IdentityUser
+    {
+        [PersonalData]
+        public DateTime DOB { get; set; }
+    }
+}
