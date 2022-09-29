7c63dd14242188975522b0454ff6d5c63b905c83
Daniel Lopes Arcanjo <60639559+danlopes-arc@users.noreply.github.com>
2022-09-29 01:18:38 -0400

    Revert "Extend identity user"

--- MvcBootstrap.App/Data/ApplicationDbContext.cs
+++ MvcBootstrap.App/Data/ApplicationDbContext.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using MvcBootstrap.App.Models;
 
 namespace MvcBootstrap.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<User>
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
--- MvcBootstrap.App/Program.cs
+++ MvcBootstrap.App/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using MvcBootstrap.App.Models;
 using MvcBootstrap.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +10,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<User>()
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
